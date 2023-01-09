5fc9976ef0ee575e02d1da630f8aab31f68b0ed2
Brownie <cristenjohnson@rocketmail.com>
2023-01-09 11:06:15 -0600

    initial commit

--- script.js
+++ script.js
@@ -11,3 +11,12 @@
 </ul>
 <img src="${}">
 */
+//set up window handler form
+//set up submit handler for the form
+//cancel submission event.preventDefault()
+//validate inputs have data in them
+//check fuel level and cargo mass; report launch status
+//make the list visible
+//fetch data
+//randomly choose planet
+//display info of chosen planet
